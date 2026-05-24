be87d42365e3d71dd4355325925168f6e1460a35
Luis Oller <difdif10@gmail.com>
2026-05-24 18:48:07 +0200

    changed port to db.py

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,7 @@ def get_conn():
     try:
         conexion = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT "),
+            port=os.getenv("DB_PORT"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
