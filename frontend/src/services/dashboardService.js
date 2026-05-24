992ff8bd84cf80cff687d6b800ef23237ec26095
Luis Oller <difdif10@gmail.com>
2026-05-24 19:22:06 +0200

    corrected confirmarReserva function

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -11,5 +11,5 @@ export const getDashboard = async () => {
 }
 
 export const confirmarReserva = async (id) => {
-    return await axios.put(`http://127.0.0.1:5000/api/dashboard/${id}`)
+    return await axios.put(`${import.meta.env.VITE_API_URL}/api/dashboard/${id}`)
 }
