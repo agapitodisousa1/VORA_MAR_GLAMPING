1f21a96a81f3be4008346a5ac61f4d840225db7b
Luis Oller <difdif10@gmail.com>
2026-05-24 19:06:20 +0200

    corrected dashboardService

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,10 +1,7 @@
 import axios from "axios"
 
+const API = "http://127.0.0.1:5000/api/dashboard/"
 
-
-axios.get(
-
-)
 export const getDashboard = async () => {
 
     const token = localStorage.getItem("token")
@@ -14,5 +11,5 @@ export const getDashboard = async () => {
 }
 
 export const confirmarReserva = async (id) => {
-    return await axios.put(`${import.meta.env.VITE_API_URL}/api/dashboard/${id}`)
+    return await axios.put(`http://127.0.0.1:5000/api/dashboard/${id}`)
 }
